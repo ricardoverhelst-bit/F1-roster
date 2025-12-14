98c8c80c66fb5d18db4877485868cfc7e492dfba
Ricardo <ricardo.verhelst@gmail.com>
2025-12-14 16:26:25 +0100

    Een team een driver model toegevoegd

---
+++ app/models.py
@@ -0,0 +1,11 @@
+class Team:
+    def __init__(self, name, country):
+        self.name = name
+        self.country = country
+
+
+class Driver:
+    def __init__(self, name, number, team_id):
+        self.name = name
+        self.number = number
+        self.team_id = team_id
