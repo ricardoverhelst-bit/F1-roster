371b93d04aab4d700023e3ba7a97c40aa51b747c
Ricardo <ricardo.verhelst@gmail.com>
2025-12-14 16:41:39 +0100

    Basis functie voor drivers toe te voegen

--- app/main.py
+++ app/main.py
@@ -19,10 +19,29 @@ def add_team():
 
     print("Team toegevoegd\n")
 
+def add_driver():
+    print("\n--- DRIVER TOEVOEGEN ---")
+
+    name = input("Naam rijder: ")
+    number = input("Rugnummer: ")
+    team_id = input("Team ID: ")
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO drivers (name, number, team_id) VALUES (?, ?, ?)",
+        (name, number, team_id)
+    )
+
+    conn.commit()
+    conn.close()
+
+    print("Driver toegevoegd\n")
 
 def main():
     create_tables()
-    add_team()
+    add_driver()
 
 
 if __name__ == "__main__":
