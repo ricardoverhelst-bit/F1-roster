230fe044b491804d7da1957e007399e31823e722
Ricardo <ricardo.verhelst@gmail.com>
2025-12-14 16:24:10 +0100

    Aanmaken tabellen database

--- app/database.py
+++ app/database.py
@@ -5,3 +5,28 @@ DB_PATH = Path("data/f1_roster.db")
 
 def get_connection():
     return sqlite3.connect(DB_PATH)
+
+def create_tables():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS teams (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        country TEXT
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS drivers (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        number INTEGER,
+        team_id INTEGER,
+        FOREIGN KEY(team_id) REFERENCES teams(id)
+    )
+    """)
+
+    conn.commit()
+    conn.close()
