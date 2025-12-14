9e95cc63aa6144d8070d34b231ca8874097269cd
Ricardo <ricardo.verhelst@gmail.com>
2025-12-14 16:39:09 +0100

    een basis functie voor teams toe te voegen

--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
-from app.database import create_tables
+from app.database import create_tables, get_connection
+
+def add_team():
+    print("\n--- TEAM TOEVOEGEN ---")
+
+    name = input("Teamnaam: ")
+    country = input("Land: ")
+
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO teams (name, country) VALUES (?, ?)",
+        (name, country)
+    )
+
+    conn.commit()
+    conn.close()
+
+    print("Team toegevoegd\n")
+
 
 def main():
     create_tables()
-    print("F1 Team Roster gestart")
+    add_team()
+
 
 if __name__ == "__main__":
     main()
